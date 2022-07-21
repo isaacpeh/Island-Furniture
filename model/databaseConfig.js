c79675c4f2cba047f983e4d5ade334368a333047
isaacpeh <68186171+isaacpeh@users.noreply.github.com>
2022-07-21 09:29:37 +0800

    Update databaseConfig.js

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "rltk2001*",
+            password: "yourownpassword",
             database: "islandfurniture-it07",
             port:3307
         });
