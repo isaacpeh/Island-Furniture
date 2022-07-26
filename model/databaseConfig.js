e123a97c972df55dedcaac42bee430c076fdcd3e
Barry <leebarry008@gmail.com>
2022-07-26 21:39:48 +0800

    Sales History fixed to pull all past orders

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "yourownpassword",
+            password: "root1234",
             database: "islandfurniture-it07"
         });
         return conn;
